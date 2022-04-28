81fbb2150f9d33742b54c55eb5eef5eafb71cfe9
Ivan <uvanes21@yandex.ru>
2022-04-28 07:00:22 +0300

    Первый коммит

---
+++ src/test/java/ru/hogwarts/schoolhogwarts/SchoolHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolhogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
